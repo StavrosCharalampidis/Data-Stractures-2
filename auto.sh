0c4756094833f72883b9b0c9c19f9e2cd579afbb
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-12-03 14:32:28 +0200

    📜 ⚙️ Automate shell script ⚙️ 📜

---
+++ auto.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+git add .vscode/*
+git commit -m "⭐️ Extensions ⭐️"
+git push -u origin main
+
+git add Icsd22239_lab03/*
+git commit -m "📚 C Exercise 📚"
+git push -u origin main
+
+git add auto.sh
+git commit -m "📜 ⚙️ Automate shell script ⚙️ 📜"
+git push -u origin main
+
+git add main
+git commit -m "🔨 ⚡⚙️ C Exe file ⚡⚙️ 🔨"
+git push -u origin main
+
+git add .
+git commit -m "R"
+git push -u origin main
